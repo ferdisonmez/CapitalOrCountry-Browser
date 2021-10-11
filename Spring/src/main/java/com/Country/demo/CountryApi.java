644dd173c8525924b4ee48af0ed035faa93a1d7e
Ferdi Sönmez <36517990+ferdisonmez@users.noreply.github.com>
2021-10-11 18:46:40 +0300

    Update CountryApi.java
    Port iletişimi için crossorigin eklendi

--- Spring/src/main/java/com/Country/demo/CountryApi.java
+++ Spring/src/main/java/com/Country/demo/CountryApi.java
@@ -3,25 +3,26 @@ package com.Country.demo;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200",maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/country")
 public class CountryApi {
 
-    @GetMapping("/lıst")
+    @GetMapping("/list")
     public ResponseEntity<?> CountryShow() throws IOException {
         final ObjectMapper objectMapper = new ObjectMapper();
         List<Country> langList = objectMapper.readValue(new File("src/main/resources/static/restcountries.json"), new TypeReference<List<Country>>(){});
-        langList.forEach(x -> System.out.println(x.toString()));
+        for (int i = 0; i <langList.size(); i++) {
+            if(langList.get(i).getCapital()==null ||langList.get(i).getCapital()==" "){
+                langList.get(i).setCapital("A");
+            }
+        }
         return ResponseEntity.ok(langList);
     }
 
